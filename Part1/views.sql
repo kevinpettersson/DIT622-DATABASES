28839d302e35a9b0afbe54dcf5eb3315cdd679f4
Kevin Pettersson <kevin.petterssons@hotmail.com>
2025-02-03 16:29:05 +0100

    fixad så vi "skapar" helper views före vi skapar pathtograduation

--- Part1/views.sql
+++ Part1/views.sql
@@ -45,6 +45,50 @@ FROM
 JOIN WaitingList AS wl ON wl.student = s.idnr 
 JOIN Courses     AS c  ON wl.course = c.code;
 
+-- creates a subset of FinishedCourses but keeping only the ones with a grade other than U.
+CREATE VIEW PassedCourses AS
+SELECT * FROM FinishedCourses
+WHERE FinishedCourses.grade <> 'U';
+
+-- returns a view of (student, courses) that have mandatory courses not yet passed
+CREATE VIEW UnreadMandatory AS
+SELECT
+    s.idnr AS student, 
+    mp.course AS course
+FROM Students AS s
+JOIN MandatoryProgram AS mp ON s.program = mp.program
+WHERE mp.course NOT IN (
+    SELECT pc.course
+    FROM PassedCourses AS pc
+    WHERE pc.student = s.idnr
+)
+
+UNION
+
+SELECT
+    s.idnr AS student,
+    mb.course
+FROM Students AS s
+JOIN StudentBranches AS sb ON s.idnr = sb.student
+JOIN MandatoryBranch AS mb ON sb.branch = mb.branch AND sb.program = mb.program
+WHERE mb.course NOT IN (
+    SELECT pc.course
+    FROM PassedCourses AS pc
+    WHERE pc.student = s.idnr
+);
+
+-- RecommendedCourses
+CREATE VIEW RecommendedCourses AS 
+SELECT 
+    pc.student AS student,
+    rb.course AS course,
+    pc.credits AS credits
+FROM PassedCourses AS pc 
+JOIN StudentBranches AS sb ON pc.student = sb.student
+JOIN RecommendedBranch AS rb ON sb.branch = rb.branch 
+                             AND sb.program = rb.program 
+                             AND pc.course = rb.course;
+
 -- PathToGraduation
 CREATE VIEW PathToGraduation AS
 WITH TotalCredits AS(
@@ -89,66 +133,6 @@ LEFT JOIN MathAndSeminar AS ms ON s.idnr = ms.student
 LEFT JOIN RecommendedCourses AS rc ON s.idnr = rc.student
 ORDER BY s.idnr;
 
-/*
-SELECT 
-    s.idnr AS student, 
-    COALESCE(SUM(pc.credits), 0) AS totalcredits,
-    COUNT(um.course) AS mandatoryleft,
-    COALESCE(SUM(CASE WHEN c.classification = 'math' THEN pc.credits END), 0) AS mathcredits,
-    COALESCE(COUNT(CASE WHEN c.classification = 'seminar' THEN 1 END), 0) AS seminarcourses,
-    COALESCE(SUM(pc.credits), 0) > 30 AND COALESCE(SUM(CASE WHEN c.classification = 'math' THEN pc.credits END), 0) > 20 AS qualified
-FROM Students AS s 
-LEFT JOIN PassedCourses AS pc ON s.idnr = pc.student
-LEFT JOIN UnreadMandatory AS um ON s.idnr = um.student
-LEFT JOIN RecommendedCourses AS rc ON s.idnr = rc.student
-LEFT JOIN Classified AS c ON c.code = pc.course AND c.classification = 'math' OR c.classification = 'seminar'
-GROUP BY s.idnr;
-*/
 
--- Helper Views --
-
--- creates a subset of FinishedCourses but keeping only the ones with a grade other than U.
-CREATE VIEW PassedCourses AS
-SELECT * FROM FinishedCourses
-WHERE FinishedCourses.grade <> 'U';
-
--- returns a view of (student, courses) that have mandatory courses not yet passed
-CREATE VIEW UnreadMandatory AS
-SELECT
-    s.idnr AS student, 
-    mp.course AS course
-FROM Students AS s
-JOIN MandatoryProgram AS mp ON s.program = mp.program
-WHERE mp.course NOT IN (
-    SELECT pc.course
-    FROM PassedCourses AS pc
-    WHERE pc.student = s.idnr
-)
-
-UNION
-
-SELECT
-    s.idnr AS student,
-    mb.course
-FROM Students AS s
-JOIN StudentBranches AS sb ON s.idnr = sb.student
-JOIN MandatoryBranch AS mb ON sb.branch = mb.branch AND sb.program = mb.program
-WHERE mb.course NOT IN (
-    SELECT pc.course
-    FROM PassedCourses AS pc
-    WHERE pc.student = s.idnr
-);
-
--- RecommendedCourses
-CREATE VIEW RecommendedCourses AS 
-SELECT 
-    pc.student AS student,
-    rb.course AS course,
-    pc.credits AS credits
-FROM PassedCourses AS pc 
-JOIN StudentBranches AS sb ON pc.student = sb.student
-JOIN RecommendedBranch AS rb ON sb.branch = rb.branch 
-                             AND sb.program = rb.program 
-                             AND pc.course = rb.course;
 
 

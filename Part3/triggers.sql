a7bcdc00161256e58d24993b7e275a688097be97
Damkilde <oskardamkilde@gmail.com>
2025-02-21 13:53:58 +0100

    NOT TESTED - Insert done

--- Part3/triggers.sql
+++ Part3/triggers.sql
@@ -21,9 +21,74 @@ else
 
 CREATE FUNCTION insertInRegistrations()
 RETURNS TRIGGER AS $$
+DECLARE 
+  maxpos int;
+  courseCapacity int;
+
 BEGIN
+
+  -- Assign value to maxpos
+  maxpos := (
+    SELECT COUNT(*)
+    FROM Registrations
+    WHERE course = NEW.course
+  );
+
+  -- Raise exception if student already registered or waiting
+  IF EXISTS ( 
+    SELECT 1 FROM Registrations
+    WHERE student = NEW.student
+    AND course = NEW.course
+  ) THEN
     RAISE EXCEPTION 'Unable to register sID % -> course %', NEW.student, NEW.course
-        USING HINT = 'Registration failed';
+    USING HINT = 'Already registered or waiting';
+  END IF;
+
+  IF NOT (
+      ( -- Raise exception if student haven't
+      SELECT COUNT(*) 
+      FROM passedcourses
+      WHERE student = NEW.student 
+      AND course IN (
+        SELECT prerequisite
+        FROM prerequisites
+        WHERE course = NEW.course
+      ) 
+      ) = (
+      SELECT COUNT(*) 
+      FROM prerequisites 
+      WHERE course = NEW.course
+      )
+  ) THEN
+      RAISE EXCEPTION 'Unable to register sID % -> course %', NEW.student, NEW.course
+      USING HINT = 'Missing prerequisites';
+  END IF;
+
+  IF EXISTS ( -- If the course is a limitedcourse
+    SELECT 1
+    FROM LimitedCourses
+    WHERE course = NEW.course
+  ) THEN -- check if full
+    SELECT capacity INTO courseCapacity
+    FROM LimitedCourses 
+    WHERE code = NEW.course;
+  END IF;
+    
+  
+  IF (
+    maxpos >= courseCapacity
+  ) THEN
+      INSERT INTO Registrations 
+      VALUES (NEW.student, NEW.course, 'waiting');
+      RETURN NEW;
+  END IF;
+
+  INSERT INTO Registrations
+    VALUES (NEW.student, NEW.course, 'registered');
+
+
+  -- No conflict -> insert
+  RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
 
